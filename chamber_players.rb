85c5c9cbe00b2d588496fd05c5559bf219ca97a8
amyvs <amydecat@yahoo.com>
2014-08-22 15:46:18 -0700

    add main menu to UI

--- chamber_players.rb
+++ chamber_players.rb
@@ -4,3 +4,34 @@ Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 ActiveRecord::Base.establish_connection(YAML::load(File.open('./db/config.yml'))['test'])
+
+def welcome
+  system('clear')
+  puts "*" * 27
+  puts "Welcome to Chamber Players!"
+  puts "*" * 27
+  main_menu
+end
+
+def main_menu
+  puts "\nPlease select from the following choices:"
+  puts "[p] to add a new piece"
+  puts "[m] to add a new musician"
+  puts "[x] to exit"
+
+  choice = gets.chomp
+  case choice
+  when 'p'
+    add_piece
+  when 'm'
+    add_musician
+  when 'x'
+    puts "\nUntil next time!"
+    exit
+  else
+    puts "Invalid option, please try again."
+    main_menu
+  end
+end
+
+welcome
