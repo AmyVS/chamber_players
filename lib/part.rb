c74d9581df35cbe0db206711a675c987e19ece06
amyvs <amydecat@yahoo.com>
2014-08-22 11:56:17 -0700

    add spacing to associations and validations in classes

--- lib/part.rb
+++ lib/part.rb
@@ -1,7 +1,9 @@
 class Part < ActiveRecord::Base
   belongs_to :piece
   has_many :roles
+
   validates :instrument, presence: true
+
   validates :instrument, format: { with: /[a-zA-Z]/,
     message: "This input only allows letters." }
 
