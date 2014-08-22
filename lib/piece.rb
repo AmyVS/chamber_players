55e8645edc2f239a265c1ad82e1be11b091a6a3b
amyvs <amydecat@yahoo.com>
2014-08-22 10:44:16 -0700

    add letter only validation for title of piece

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,4 +1,7 @@
 class Piece < ActiveRecord::Base
   has_many :parts
   validates :title, :composer, :number_of_parts, presence: true
+  validates :title, format: { with: /\A[a-zA-Z]+\z/,
+    message: "This input only allows letters." }
+
 end
--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -5,4 +5,10 @@ describe Piece do
   it { should validate_presence_of :title }
   it { should validate_presence_of :composer }
   it { should validate_presence_of :number_of_parts }
+
+  it 'ensures that the title only accepts letters' do
+    piece = Piece.create(title: 12, composer: 'Bach', number_of_parts: 4)
+    expect(piece.save).to eq false
+  end
+
 end
