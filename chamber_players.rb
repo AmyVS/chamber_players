7d8ae958dc4cd46f6ad4e8a604dbfca2719eb6bc
amyvs <amydecat@yahoo.com>
2014-08-22 16:09:31 -0700

    add method add_piece to UI

--- chamber_players.rb
+++ chamber_players.rb
@@ -29,8 +29,40 @@ def main_menu
     puts "\nUntil next time!"
     exit
   else
-    puts "Invalid option, please try again."
+    puts "\nInvalid option, please try again."
     main_menu
+  end
+end
+
+def add_piece
+  puts "\nPlease enter the title of the piece:"
+  title = gets.chomp
+
+  puts "\nPlease enter the composer of the piece:"
+  composer = gets.chomp
+
+  puts "\nPlease enter the number of parts for this piece:"
+  number_of_parts = gets.chomp
+
+  new_piece = Piece.create(title: title, composer: composer, number_of_parts: number_of_parts.to_i)
+  if new_piece.save
+    puts "\n#{new_piece.title} by #{new_piece.composer} has been added to the music library."
+    puts "\nWould you like to enter a new piece? y/n"
+
+    choice = gets.chomp
+    case choice
+    when 'y'
+      add_piece
+    when 'n'
+      puts "\nReturning to the main menu..."
+      main_menu
+    else
+      puts "\nInvalid entry, returning to the main menu."
+      main_menu
+  else
+    puts "Sorry, that wasn't a valid entry. Please try again."
+    new_piece.errors.full_messages.each { |message| puts message }
+    add_piece
   end
 end
 
