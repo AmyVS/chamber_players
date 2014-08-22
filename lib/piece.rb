4202d7f36a09b888c1fa39dd2fd5855c4c0021eb
amyvs <amydecat@yahoo.com>
2014-08-22 10:25:24 -0700

    add validation of title for piece

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,3 +1,4 @@
 class Piece < ActiveRecord::Base
   has_many :parts
+  validates :title, presence: true
 end
--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe Piece do
   it { should have_many :parts }
+  it { should validate_presence_of :title }
 end
