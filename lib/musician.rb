c26ae70409f51dafbf8d02e08166cecd262bc1ad
amyvs <amydecat@yahoo.com>
2014-08-22 10:22:50 -0700

    add validation of name for musician

--- lib/musician.rb
+++ lib/musician.rb
@@ -1,3 +1,4 @@
 class Musician < ActiveRecord::Base
   has_many :roles
+  validates :name, presence: true
 end
--- spec/musician_spec.rb
+++ spec/musician_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe Musician do
   it { should have_many :roles }
+  it { should validate_presence_of :name }
 end
