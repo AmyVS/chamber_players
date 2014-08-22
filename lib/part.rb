12462f39e4e48162a7eac1b3a8e140308ec63af0
amyvs <amydecat@yahoo.com>
2014-08-22 10:40:14 -0700

    add letter only validation for instrument of part

--- lib/part.rb
+++ lib/part.rb
@@ -2,4 +2,7 @@ class Part < ActiveRecord::Base
   belongs_to :piece
   has_many :roles
   validates :instrument, presence: true
+  validates :instrument, format: { with: /\A[a-zA-Z]+\z/,
+    message: "This input only allows letters." }
+
 end
--- spec/part_spec.rb
+++ spec/part_spec.rb
@@ -4,4 +4,10 @@ describe Part do
   it { should belong_to :piece }
   it { should have_many :roles }
   it { should validate_presence_of :instrument }
+
+  it 'ensures that the instrument only accepts letters' do
+    part = Part.create(instrument: 12, piece_id: 1)
+    expect(part.save).to eq false
+  end
+
 end
