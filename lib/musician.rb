f879d9f8d2fa0da3219f4012349f7d8cf98fb54b
amyvs <amydecat@yahoo.com>
2014-08-22 10:38:20 -0700

    add letter only validation for instrument of musician

--- lib/musician.rb
+++ lib/musician.rb
@@ -1,7 +1,7 @@
 class Musician < ActiveRecord::Base
   has_many :roles
   validates :name, :instrument, presence: true
-  validates :name, format: { with: /\A[a-zA-Z]+\z/,
+  validates :name, :instrument, format: { with: /\A[a-zA-Z]+\z/,
     message: "This input only allows letters." }
 
 end
--- spec/musician_spec.rb
+++ spec/musician_spec.rb
@@ -5,8 +5,13 @@ describe Musician do
   it { should validate_presence_of :name }
   it { should validate_presence_of :instrument }
 
-  it 'ensures that the name only has letters' do
+  it 'ensures that the name only accepts letters' do
     musician = Musician.create(name: 12, instrument: 'bassoon')
+    expect(musician.save).to eq false
+  end
+
+  it 'ensures that the instrument only accepts letters' do
+    musician = Musician.create(name: 'Chris', instrument: 12)
     expect(musician.save).to eq false
   end
 
