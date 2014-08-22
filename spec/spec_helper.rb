77422614760366c17c464267e402255928993c2b
amyvs <amydecat@yahoo.com>
2014-08-22 09:22:50 -0700

    add files to lib and spec directories

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+require 'bundler/setup'
+Bundler.require(:default, :test)
+
+Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each { |file| require file }
+
+ActiveRecord::Base.establish_connection(YAML::load(File.open('./db/config.yml'))['development'])
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Chamber_piece.all.each { |piece| piece.destroy }
+    Musician.all.each { |musician| musician.destroy }
+    Part.all.each { |part| part.destroy }
+    Role.all.each { |role| role.destroy }
+  end
+end
