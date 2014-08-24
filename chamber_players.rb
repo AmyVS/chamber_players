60d294498ffebcddd0c3ef18d619f94b27ac2ae6
amyvs <amydecat@yahoo.com>
2014-08-24 12:10:27 -0700

    update UI with new Composer class functionality

--- chamber_players.rb
+++ chamber_players.rb
@@ -6,6 +6,9 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 ActiveRecord::Base.establish_connection(YAML::load(File.open('./db/config.yml'))['development'])
 
+@current_composer = nil
+@current_piece = nil
+
 def welcome
   system('clear')
   puts "*" * 27
@@ -48,10 +51,11 @@ def add_piece
   puts "\nPlease enter the number of parts for this piece:"
   number_of_parts = gets.chomp
 
-  new_piece = Piece.create(title: title, composer: composer, number_of_parts: number_of_parts)
+  new_composer = Composer.create(:name composer)
+  new_piece = Piece.create(title: title, composer: new_composer.id, number_of_parts: number_of_parts)
 
-  if new_piece.save
-    puts "\n#{new_piece.title} by #{new_piece.composer} has been added to the music library."
+  if new_piece.save && new_composer.save
+    puts "\n#{new_piece.title} by #{new_composer.name} has been added to the music library."
     puts "\nWould you like to enter a new piece? y/n"
     choice = gets.chomp
     case choice
@@ -66,7 +70,11 @@ def add_piece
     end
   else
     puts "\nSorry, that wasn't a valid entry. Please try again."
-    new_piece.errors.full_messages.each { |message| puts message }
+    if !new_piece
+      new_piece.errors.full_messages.each { |message| puts message }
+    elsif !new_composer
+      new_composer.errors.full_messages.each { |message| puts message }
+    end
     add_piece
   end
 end
@@ -74,13 +82,13 @@ end
 def parts
   puts "\nPlease enter a name of a composer, to help locate a piece."
   composer = gets.chomp
-  # This part still needs work...
-  results = Piece.where(:composer == composer.capitalize)
+  @current_composer = Composer.where(:name == composer.capitalize)
 
-  puts "\nHere are all of our pieces composed by #{composer}:"
-  results.each_with_index do |piece, index|
-    puts "#{index+1}. #{piece.title} -- #{piece.number_of_parts} parts."
-  end
+  # do a scope
+  # puts "\nHere are all of our pieces composed by #{@current_composer.name}:"
+  # results.each_with_index do |piece, index|
+  #   puts "#{index+1}. #{piece.title} -- #{piece.number_of_parts} parts."
+  # end
 
   puts "\nWhich one would you like to access? Please enter the appropriate number, or [x] to return to the main menu."
   choice = gets.chomp
